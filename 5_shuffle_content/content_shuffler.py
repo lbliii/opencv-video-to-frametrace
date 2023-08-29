59683027b29acce1b09d292f40c3a507c65ba938
L.B <mrlawrencelane@gmail.com>
2023-08-29 17:06:03 -0400

    checkpoint

---
+++ 5_shuffle_content/content_shuffler.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+import shutil
+
+def shuffle_content(input_path, output_path):
+    # create an originals and edges directory in the output path
+    originals_output_path = f"{output_path}/originals"
+    if not os.path.exists(originals_output_path):
+        os.makedirs(originals_output_path)
+
+    edges_output_path = f"{output_path}/edges"
+    if not os.path.exists(edges_output_path):
+        os.makedirs(edges_output_path)
+
+    for dirpath, dirs, files in os.walk(input_path):
+        for file in files:
+            if "frame" and "edges" not in file:
+                # Copy the original image to the originals directory
+                shutil.copy(f"{dirpath}/{file}", originals_output_path)
+            elif "frame" not in file and "edges" in file:
+                # Copy the images and gifs to the edges directory
+                shutil.copy(f"{dirpath}/{file}", edges_output_path)
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='content_montager.py',
+        description='Convert images and gifs into a collage'
+    )
+    parser.add_argument('-i', '--input', nargs='+', required=True, help='Input image directory')
+    parser.add_argument('-o', '--output', required=True, help='Output image directory')
+    args = parser.parse_args()
+
+    print(f"Input: {args.input} \nOutput: {args.output}\n")
+
+    total_inputs = len(args.input)
+    current_input = 0
+    for input_path in args.input:
+        try:
+            current_input += 1
+            print(f"{input_path}; {current_input}/{total_inputs}")
+            shuffle_content(input_path, args.output)
+        except Exception as e:
+            print(f"Exception: {e}")
+
+if __name__ == "__main__":
+    main()
---
+++ 6_montage_content/content_montager.py
@@ -0,0 +1,82 @@
+import os
+import argparse
+from bs4 import BeautifulSoup
+import shutil
+
+html_template = """
+<html>
+<head>
+<title>Content Montage</title>
+</head>
+<body>
+<h1>Content Montage</h1>
+<div id="collage-container">
+</div>
+</body>
+</html>
+"""
+
+def create_html_page(output_path):
+    index_path = os.path.join(output_path, "collage", "index.html")
+    
+    if not os.path.exists(os.path.join(output_path, "collage")):
+        os.makedirs(os.path.join(output_path, "collage"))
+
+    if not os.path.exists(index_path):
+        with open(index_path, "w") as f:
+            f.write(html_template)
+
+def append_image_to_html_page(output_path, image_path):
+    index_path = os.path.join(output_path, "collage", "index.html")
+
+    with open(index_path, "r") as f:
+        html_content = f.read()
+
+    soup = BeautifulSoup(html_content, "html.parser")
+    collage_div = soup.find("div", id="collage-container")
+
+    if collage_div:
+        img_tag = soup.new_tag("img", src=image_path, width="200", height="200")
+        collage_div.append(img_tag)
+
+        with open(index_path, "w") as f:
+            f.write(str(soup))
+
+def process_content(input_path, output_path):
+    try:
+        # Create the HTML page
+        create_html_page(output_path)
+
+        # Create the output directory /collage/static
+        static_output_path = os.path.join(output_path, "collage", "static")
+        if not os.path.exists(static_output_path):
+            os.makedirs(static_output_path)
+
+        for dirpath, _, files in os.walk(input_path):
+            sorted_files = sorted(files)
+            for file in sorted_files:
+                print(f"Copying {file} to {static_output_path}")
+                shutil.copy(os.path.join(dirpath, file), os.path.join(static_output_path, file))
+                append_image_to_html_page(output_path, f"./static/{file}")
+
+    except Exception as e:
+        print(f"Exception: {e}")
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='content_montager.py',
+        description='Convert images and gifs into a collage'
+    )
+    parser.add_argument('-i', '--input', nargs='+', required=True, help='Input image directory')
+    parser.add_argument('-o', '--output', required=True, help='Output image directory')
+    args = parser.parse_args()
+
+    print(f"Input: {args.input} \nOutput: {args.output}\n")
+
+    try:
+        process_content(args.input[0], args.output)
+    except Exception as e:
+        print(f"Exception: {e}")
+
+if __name__ == "__main__":
+    main()
